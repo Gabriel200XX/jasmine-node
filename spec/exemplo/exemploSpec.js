924796b40f01ed0169789ed2cebc265826397772
Gabriel Dezan Busarello <gabriel20053@gmail.com>
2018-04-03 11:48:21 -0300

    Primeiro Commit

---
+++ spec/exemplo/exemploSpec.js
@@ -0,0 +1,8 @@
+describe("Suite de teste de exemplo com NodeJS", function() {
+  it("Deve demonstrar o uso do Jasmine com o NodeJS", function() {
+    expect(true).toEqual(true);
+  });
+  it("Deve demonstrar o uso do Karma em modo de escuta", function() {
+    expect(1).toEqual(1);
+  });
+});
